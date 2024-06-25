1470d138a082a8be9039e6b47a28b5d37f43eee9
Jamie897 <jamiedanielle456@gmail.com>
2024-06-25 17:49:46 -0500

    .

--- holbegram/lib/main.dart
+++ holbegram/lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 
-void main() {
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
